eb4c6a87a606a64304ecc04c6cad15d46db2f3e1
U88478 <chelovek.tutu@gmail.com>
2022-07-25 15:50:44 +0300

    telebot

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import os
-from aiogram import Bot, Dispatcher, executor
+# from aiogram import Bot, Dispatcher, executor
+import telebot
 import logging
 from config import *
+from flask import Flask, request
 
-bot = Bot(token, parse_mode="HTML")
-dp = Dispatcher(bot)
-logging.basicConfig(level=logging.DEBUG)
+# bot = Bot(token, parse_mode="HTML")
+# dp = Dispatcher(bot)
+bot = telebot.TeleBot(token)
+server = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
 
-@dp.message_handler(commands='start', commands_prefix='/')
-async def start(message):
-    ans = await message.reply("Доброго пожалувать, виход там 👉")
-    message.delete()
-    ans.delete()
+@bot.message_handler(commands='start', commands_prefix='/')
+def start(message):
+    await message.reply("Доброго пожалувать, виход там 👉")
+
+@server.route(f'/{token}', methods='POST')
+def redirect_message():
+    json_str = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return '!', 200
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    bot.remove_webhool()
+    bot.set_webhook(url=app_url)
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    # executor.start_polling(dp)
